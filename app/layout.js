9d67d022d42ccdd5247b652412d93a085c443a4f
Dawood0426 <rd535328@gmail.com>
2025-04-22 09:50:04 -0700

    kuch change kuata
    change

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,10 @@ export default function RootLayout({ children }) {
      crossorigin="anonymous"></script>
 
 
-<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-V78YVZ0GXM"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)};
   gtag('js', new Date());
 
   gtag('config', 'G-V78YVZ0GXM');
